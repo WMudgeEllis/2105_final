a8f21d464b5a38dbae908aa62967753a87655464
Jessica Jones <daddehjones@gmail.com>
2021-06-16 11:40:33 -0400

    initialize

--- lib/train.rb
+++ lib/train.rb
@@ -1,10 +1,12 @@
 class Train
 
   attr_reader :name,
-              :type
-            
+              :type,
+              :cargo
+
   def initialize(info)
     @name = info[:name]
     @type = info[:type]
+    @cargo = {}
   end
 end
