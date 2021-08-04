2ffc3066edc546e4963e4789a7e7a3b4507d61f0
Weston Ellis <84806907+WMudgeEllis@users.noreply.github.com>
2021-08-04 16:31:37 -0600

    Update car_spec.rb

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,18 +1,1 @@
-require './lib/car'
-require './lib/train'
 
-RSpec.describe Car do
-  it "exists" do
-    car1 = Car.new({type: 'Mail', weight: 5})
-
-    expect(car1).to be_an_instance_of(Car)
-  end
-
-  it "has attributes" do
-    car1 = Car.new({type: 'Mail', weight: 5})
-
-    expect(car1.type).to eq('Mail')
-    expect(car1.weight).to eq(5)
-
-  end
-end
